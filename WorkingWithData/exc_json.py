227608c7acf74b8bab83907c92922445d4af21e7
RadoslawSajdak <sajdak.radoslaw@gmail.com>
2021-10-17 21:52:12 +0200

    Reading json file

--- WorkingWithData/exc_json.py
+++ WorkingWithData/exc_json.py
@@ -2,18 +2,24 @@
 
 import json
 
-x = {
-    "name": "Piotr",
-    "surname": "Kalias",
-    "age": 21,
-    "student": True,
-    "skills": ("C++", "Python"),
-    "favourites": [
-        {"fruit": "Banana"},
-        {"color": "Green"}
-    ]
-}
+FIRST_WRITE = False
+if FIRST_WRITE:
+    x = {
+        "name": "Piotr",
+        "surname": "Kalias",
+        "age": 21,
+        "student": True,
+        "skills": ("C++", "Python"),
+        "favourites": [
+            {"fruit": "Banana"},
+            {"color": "Green"}
+        ]
+    }
 
-print(json.dumps(x, indent = 4))
-with open('file.json', 'w') as outfile:
-    json.dump(x, outfile, indent = 4)
+    print(json.dumps(x, indent = 4))
+    with open('file.json', 'w') as outfile:
+        json.dump(x, outfile, indent = 4)
+else:
+    with open('file.json') as json_file:
+       data = json.load(json_file)
+    print(json.dumps(data, indent = 2))
