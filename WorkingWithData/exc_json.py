5f334936225e5d4a0ba3ea263e31944489b50833
RadoslawSajdak <sajdak.radoslaw@gmail.com>
2021-10-17 22:37:18 +0200

    Working with json file

--- WorkingWithData/exc_json.py
+++ WorkingWithData/exc_json.py
@@ -2,24 +2,37 @@
 
 import json
 
-FIRST_WRITE = False
-if FIRST_WRITE:
-    x = {
-        "name": "Piotr",
-        "surname": "Kalias",
-        "age": 21,
-        "student": True,
-        "skills": ("C++", "Python"),
-        "favourites": [
-            {"fruit": "Banana"},
-            {"color": "Green"}
-        ]
-    }
 
-    print(json.dumps(x, indent = 4))
+with open('file.json') as json_file:
+    data = json.load(json_file)
+print(json.dumps(data, indent = 2))
+    
+result = int(input("0. Remove 1. Write\n"))
+
+## Removing ##
+if( 0 == result):
+    element = int(input("Element to remove (0-n):"))
+    try:
+        data.pop(element)
+    except IndexError:
+        print("Index out of range!")
+
     with open('file.json', 'w') as outfile:
-        json.dump(x, outfile, indent = 4)
+        json.dump(data, outfile, indent = 2)
+
+    print(data)
+## Adding ##
+elif( 1 == result ):
+    print("Adding")
+    data.append({})
+    data[-1]["name"] = input("Name: ")
+    data[-1]["surname"] = input("Surname: ")
+    data[-1]["age"] = int(input("Age: "))
+    data[-1]["student"] = bool(input("Student (True/False): "))
+    data[-1]["Skills"] = input("Skills (separated with ','): ").split(',')
+    print(data[-1])
+
+    with open('file.json', 'w') as outfile:
+        json.dump(data, outfile, indent = 2)
 else:
-    with open('file.json') as json_file:
-       data = json.load(json_file)
-    print(json.dumps(data, indent = 2))
+    print("Wrong input!")
